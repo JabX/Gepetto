13ae29645fe16d1724480fe3dda9fae2508d3ef3
JabX <damien.frikha@outlook.com>
2015-08-17 13:47:15 +0200

    Add request "live" feedback

--- Gepetto.Web/Client.fs
+++ Gepetto.Web/Client.fs
@@ -8,20 +8,41 @@ open WebSharper.UI.Next.Html
 
 [<JavaScript>]
 module Client =
+    type Status = 
+        | Empty
+        | Working
+        | Done
+
     let Main () =
         let input = inputAttr [attr.value ""] []
-        let output = h1 []
+        let answer = h1 []
+        let status = Var.Create Empty
+
+        let output (st: Var<Status>) =
+            View.FromVar st
+            |> View.Map (fun stat -> 
+                match stat with
+                | Empty -> div []
+                | Working -> h4 [text "Recherche en cours..."]
+                | Done -> div [
+                            h4 [text "La réponse :"]
+                            div [answer]
+                        ])
+            |> Doc.EmbedView
+            
         div [
+            h2 [text "Les chiffres de Gepetto"]
             input
             buttonAttr [
                 on.click (fun _ _ ->
+                    status.Value <- Working
                     async {
                         let! data = Server.GetScore input.Value
-                        output.Text <- sprintf "%i%%" data
+                        answer.Text <- sprintf "%i%%" data
+                        status.Value <- Done
                     }
                     |> Async.Start
                 )
             ] [text "Poser la question !"]
-            h4 [text "La réponse :"]
-            div [output]
+            output status
         ]
--- Gepetto.Web/Main.fs
+++ Gepetto.Web/Main.fs
@@ -4,49 +4,26 @@ open WebSharper
 open WebSharper.Sitelets
 open WebSharper.UI.Next
 open WebSharper.UI.Next.Server
+open WebSharper.UI.Next.Html
 
 type EndPoint =
     | [<EndPoint "/">] Home
-    | [<EndPoint "/about">] About
 
 module Templating =
-    open WebSharper.UI.Next.Html
-
     type MainTemplate = Templating.Template<"Main.html">
 
-    // Compute a menubar where the menu item for the given endpoint is active
-    let MenuBar (ctx: Context<EndPoint>) endpoint : Doc list =
-        let ( => ) txt act =
-             liAttr [if endpoint = act then yield attr.``class`` "active"] [
-                aAttr [attr.href (ctx.Link act)] [text txt]
-             ]
-        [
-            "Home" => EndPoint.Home
-            "About" => EndPoint.About
-        ]
-
     let Main ctx action title body =
-        Content.Doc(
-            MainTemplate.Doc(
+        Content.Doc (
+            MainTemplate.Doc (
                 title = title,
-                menubar = MenuBar ctx action,
                 body = body
             )
         )
 
 module Site =
-    open WebSharper.UI.Next.Html
-
     let HomePage ctx =
-        Templating.Main ctx EndPoint.Home "Home" [
-            h1 [text "Les chiffres de Gepetto !"]
-            div [client <@ Client.Main() @>]
-        ]
-
-    let AboutPage ctx =
-        Templating.Main ctx EndPoint.About "About" [
-            h1 [text "Deuxième page qui sert à rien"]
-            p [text "Coucou"]
+        Templating.Main ctx EndPoint.Home "Les chiffres de Gepetto" [
+            client <@ Client.Main() @>
         ]
 
     [<Website>]
@@ -54,5 +31,4 @@ module Site =
         Application.MultiPage (fun ctx endpoint ->
             match endpoint with
             | EndPoint.Home -> HomePage ctx
-            | EndPoint.About -> AboutPage ctx
         )
