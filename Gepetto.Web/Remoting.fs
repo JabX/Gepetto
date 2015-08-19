a862378b72df6b6304ee41fcc1d19030f1a3f00b
JabX <damien.frikha@outlook.com>
2015-08-19 15:55:28 +0200

    Upgrade tagging, with clearer errors

--- Gepetto.Web/Remoting.fs
+++ Gepetto.Web/Remoting.fs
@@ -11,10 +11,13 @@ module Server =
                 match Score.Get sentence with
                 | Score.Percentage x -> sprintf "%i%%" x
                 | Score.Amount x -> (match x with
-                                        | Score.HowMany.All  -> "Tout le monde"
-                                        | Score.HowMany.Most -> "La plupart"
+                                        | Score.HowMany.All  -> "Vraiment beaucoup"
+                                        | Score.HowMany.Most -> "Une grosse majorité"
                                         | Score.HowMany.Some -> "Un certain nombre"
                                         | Score.HowMany.Few  -> "Quelques-uns"
-                                        | _                  -> "Personne")
-                | Score.Error -> "La question est mal formulée !"
+                                        | _                  -> "Quasiment aucun")
+                | Score.Error x -> (match x with
+                                    | Score.ErrorType.Sentence     -> "La question est mal formulée !"
+                                    | Score.ErrorType.QuestionType -> "Gepetto ne sait pas répondre à ça..."
+                                    | _                            -> "Prout")
         }
--- Gepetto.exe/Program.fs
+++ Gepetto.exe/Program.fs
@@ -8,11 +8,15 @@ module Program =
             match System.Console.ReadLine () |> Score.Get with
             | Score.Percentage x -> printfn "%i%%" x
             | Score.Amount x -> (match x with
-                                 | Score.HowMany.All  -> "Tout le monde"
-                                 | Score.HowMany.Most -> "La plupart"
-                                 | Score.HowMany.Some -> "Un certain nombre"
-                                 | Score.HowMany.Few  -> "Quelques-uns"
-                                 | _                  -> "Personne")
+                                    | Score.HowMany.All  -> "Vraiment beaucoup"
+                                    | Score.HowMany.Most -> "Une grosse majorité"
+                                    | Score.HowMany.Some -> "Un certain nombre"
+                                    | Score.HowMany.Few  -> "Quelques-uns"
+                                    | _                  -> "Quasiment aucun")
                                 |> printfn "%s"
-            | Score.Error -> printfn "La question est mal formulée !"
+            | Score.Error x -> (match x with
+                                    | Score.ErrorType.Sentence     -> "La question est mal formulée !"
+                                    | Score.ErrorType.QuestionType -> "Gepetto ne sait pas répondre à ça..."
+                                    | _                            -> "Prout")
+                                |> printfn "%s"
         0
